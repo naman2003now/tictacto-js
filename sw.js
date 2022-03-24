77689e490bfe0c4b6b2555b10813b673075755ba
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-03-24 11:26:34 +0530

    added the .

--- sw.js
+++ sw.js
@@ -1,31 +1,29 @@
 // Storing cache
-const cacheName = 'cache-v1';
+const cacheName = "cache-v1"
 const resourcesToPrecache = [
-    '/',
-    'index.html',
-    'script.js',
-    'assets/xo_icon.svg',
-    'assets/stylesheet.css'
+	"./",
+	"index.html",
+	"script.js",
+	"assets/xo_icon.svg",
+	"assets/stylesheet.css",
 ]
 
 // Loading Cache
-self.addEventListener('install', event => {
-    console.log("install event!");
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache=> {
-            return cache.addAll(resourcesToPrecache);
-        })
-    )
+self.addEventListener("install", (event) => {
+	console.log("install event!")
+	event.waitUntil(
+		caches.open(cacheName).then((cache) => {
+			return cache.addAll(resourcesToPrecache)
+		})
+	)
 })
 
-self.addEventListener('activate', event => {
-    console.log("activate event!");
+self.addEventListener("activate", (event) => {
+	console.log("activate event!")
 })
 
-self.addEventListener('fetch', event => {
-    event.respondsWith(caches.match(event.request))
-    .then(cachedResponse => {
-        return cachedResponse || fetch(event.request);
-    })
+self.addEventListener("fetch", (event) => {
+	event.respondsWith(caches.match(event.request)).then((cachedResponse) => {
+		return cachedResponse || fetch(event.request)
+	})
 })
