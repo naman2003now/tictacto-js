cf163163abe6d8e1549ab0b23fa0dd01501922e3
Yashaswini Shivathaya <83407675+y-ashaswini@users.noreply.github.com>
2022-03-25 19:48:26 +0400

    corrected typo

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
-    event.respondsWith(caches.match(event.request))
+    event.respondWith(caches.match(event.request))
     .then(cachedResponse => {
         return cachedResponse || fetch(event.request);
     })
